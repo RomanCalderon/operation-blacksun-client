6e4981c2df3998c6c425c165d4aeba9d99c35983
Roman Calderon <romanc647@gmail.com>
2020-06-09 22:23:39 -0400

    Player Health and Taking Damage

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Transform cameraTransform;
+
+    private void Update ()
+    {
+        if ( Input.GetKeyDown ( KeyCode.Mouse0 ) )
+        {
+            ClientSend.PlayerShoot ( cameraTransform.forward );
+        }
+    }
+
     private void FixedUpdate ()
     {
         SendInputToServer ();
