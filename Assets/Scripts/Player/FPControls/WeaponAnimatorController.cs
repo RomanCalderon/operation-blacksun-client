59d114305b3113b31634fff0c5970fa9d0b7127a
Roman Calderon <romanc647@gmail.com>
2020-07-10 20:33:34 -0400

    Imported Eveningstar (Sniper) model
    + Added simple weapon switching behavior for testing

--- Assets/Scripts/Player/FPControls/WeaponAnimatorController.cs
+++ Assets/Scripts/Player/FPControls/WeaponAnimatorController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,21 +11,70 @@ public class WeaponAnimatorController : MonoBehaviour
     private void OnEnable ()
     {
         WeaponsController.OnSetTrigger += SetTrigger;
+        WeaponsController.OnSetBool += SetBool;
     }
 
     private void OnDisable ()
     {
         WeaponsController.OnSetTrigger -= SetTrigger;
+        WeaponsController.OnSetBool -= SetBool;
     }
 
     // Start is called before the first frame update
-    void Start()
+    private void Awake ()
     {
-        
+        Debug.Assert ( m_animator != null, "m_animator is null." );
     }
 
     private void SetTrigger ( string parameterName )
     {
+        if ( !m_animator.isInitialized || !AnimatorHasParameter ( parameterName ) )
+        {
+            return;
+        }
         m_animator.SetTrigger ( parameterName );
+    }
+    private void SetBool ( string parameterName, bool value )
+    {
+        if ( !m_animator.isInitialized || !AnimatorHasParameter ( parameterName ) )
+        {
+            return;
+        }
+        m_animator.SetBool ( parameterName, value );
+    }
+
+    /// <summary>
+    /// Returns true if m_animator has a parameter with name <paramref name="parameterName"/>.
+    /// </summary>
+    /// <param name="parameterName">The name of the parameter.</param>
+    /// <returns></returns>
+    private bool AnimatorHasParameter ( string parameterName )
+    {
+        AnimatorControllerParameter [] parameters = m_animator.parameters;
+
+        foreach ( AnimatorControllerParameter parameter in parameters )
+        {
+            if ( parameter.name == parameterName )
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private float GetAnimationClipLength ( string clipName )
+    {
+        //Animator.StringToHash ( "" )
+
+        RuntimeAnimatorController animController = m_animator.runtimeAnimatorController;
+        for ( int i = 0; i < animController.animationClips.Length; i++ )
+        {
+            if ( animController.animationClips [ i ].name == clipName )
+            {
+                return animController.animationClips [ i ].length;
+            }
+        }
+        Debug.LogWarning ( $"Couldn't find clip with name [{clipName}]" );
+        return 0f;
     }
 }
--- Assets/Scripts/Player/FPControls/WeaponsController.cs
+++ Assets/Scripts/Player/FPControls/WeaponsController.cs
@@ -1,19 +1,44 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponsController : MonoBehaviour
 {
-    public delegate void WeaponAnimatorHandler ( string parameterName );
-    public static WeaponAnimatorHandler OnSetTrigger;
+    public enum Weapons
+    {
+        Primary,
+        Secondary
+    }
 
+    public delegate void WeaponAnimatorHandler_Trigger ( string parameterName );
+    public static WeaponAnimatorHandler_Trigger OnSetTrigger;
+    public delegate void WeaponAnimatorHandler_Bool ( string parameterName, bool value );
+    public static WeaponAnimatorHandler_Bool OnSetBool;
+    public delegate void WeaponSwitchHandler ( /*Action<float> finishCallback*/ );
+    public static WeaponSwitchHandler OnSwitchWeapon;
+
+    [Header ( "Weapons" )]
+    private Weapons m_activeWeapon = Weapons.Primary;
     [SerializeField]
     private GameObject m_primaryWeaponHolder = null;
     [SerializeField]
     private GameObject m_secondaryWeaponHolder = null;
 
+    private Coroutine m_weaponSwitchCoroutine = null;
+
     [SerializeField]
     private Transform m_cameraTransform = null;
+
+    private void OnEnable ()
+    {
+        WeaponStateBehaviour.OnStateEntered += SwitchWeapons;
+    }
+
+    private void OnDisable ()
+    {
+        WeaponStateBehaviour.OnStateEntered -= SwitchWeapons;
+    }
 
     // Start is called before the first frame update
     void Start ()
@@ -25,19 +50,72 @@ public class WeaponsController : MonoBehaviour
     // Update is called once per frame
     void Update ()
     {
+        // Weapon switching - Primary
+        if ( Input.GetKeyDown ( KeyCode.Alpha1 ) && m_activeWeapon != Weapons.Primary )
+        {
+            OnSetTrigger?.Invoke ( "Holster" );
+            //OnSwitchWeapon?.Invoke ( SwitchWeapons );
+        }
+        // Weapon switching - Secondary
+        if ( Input.GetKeyDown ( KeyCode.Alpha2 ) && m_activeWeapon != Weapons.Secondary )
+        {
+            OnSetTrigger?.Invoke ( "Holster" );
+            //OnSwitchWeapon?.Invoke ( SwitchWeapons );
+        }
+
+        // Shooting
         if ( Input.GetKeyDown ( KeyCode.Mouse0 ) )
         {
             ClientSend.PlayerShoot ( m_cameraTransform.forward );
 
             // This will be changed
             OnSetTrigger?.Invoke ( "Shoot" );
+            OnSetTrigger?.Invoke ( "BoltCharge" );
         }
+
 
         if ( Input.GetKeyDown ( KeyCode.R ) )
         {
             OnSetTrigger?.Invoke ( "ReloadFull" );
         }
+        if ( Input.GetKeyDown ( KeyCode.H ) )
+        {
+            OnSetTrigger?.Invoke ( "Holster" );
+        }
 
         // TODO: send shoot logic to server and handle response somewhere here
     }
+
+    private void SwitchWeapons ( AnimatorStateInfo stateInfo, int layerIndex )
+    {
+        if ( m_weaponSwitchCoroutine != null )
+        {
+            StopCoroutine ( m_weaponSwitchCoroutine );
+        }
+        m_weaponSwitchCoroutine = StartCoroutine ( SwitchWeaponsDelay ( stateInfo.length ) );
+    }
+
+    private IEnumerator SwitchWeaponsDelay ( float delay )
+    {
+        yield return new WaitForSeconds ( delay );
+
+        DisableWeapons ();
+        if ( m_activeWeapon == Weapons.Primary )
+        {
+            m_secondaryWeaponHolder.SetActive ( true );
+            m_activeWeapon = Weapons.Secondary;
+        }
+        else
+        {
+            m_primaryWeaponHolder.SetActive ( true );
+            m_activeWeapon = Weapons.Primary;
+        }
+    }
+
+    private void DisableWeapons ()
+    {
+        m_primaryWeaponHolder.SetActive ( false );
+        m_secondaryWeaponHolder.SetActive ( false );
+    }
+
 }
