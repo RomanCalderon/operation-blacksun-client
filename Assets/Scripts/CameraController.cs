2f8a4f485a88dd5a9d10da7f26090cc80fbfb847
Roman Calderon <romanc647@gmail.com>
2020-08-14 18:25:51 -0400

    Player look sensitivity updates based on AimController.AimState
    + Reduced normal sensitivity

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,12 +6,28 @@ public class CameraController : MonoBehaviour
 {
     public static CameraController Instance;
 
-    public Player player;
-    public float sensitivity = 100f;
-    public float clampAngle = 85f;
+    [SerializeField]
+    private Player player;
 
-    private float verticalRotation;
-    private float horizontalRotation;
+    [SerializeField]
+    private float m_clampAngle = 85f;
+
+    private float m_sensitivity;
+    private float m_normalSensitivity;
+    private float m_aimSensitivity;
+    private float m_verticalRotation;
+    private float m_horizontalRotation;
+
+
+    private void OnEnable ()
+    {
+        AimController.OnAimStateUpdated += SetSensitivity;
+    }
+
+    private void OnDisable ()
+    {
+        AimController.OnAimStateUpdated -= SetSensitivity;
+    }
 
     private void Start ()
     {
@@ -20,10 +36,24 @@ public class CameraController : MonoBehaviour
             Instance = this;
         }
 
-        verticalRotation = transform.localEulerAngles.x;
-        horizontalRotation = player.transform.eulerAngles.y;
+        InitializeSensitivity ();
+
+        m_verticalRotation = transform.localEulerAngles.x;
+        m_horizontalRotation = player.transform.eulerAngles.y;
 
         SetCursorMode ( CursorLockMode.Locked );
+    }
+
+    private void InitializeSensitivity ()
+    {
+        // Set Sens
+        // TODO: Set sens from settings
+        m_sensitivity = m_normalSensitivity = 125f;
+
+        // Set Aim Sens
+        // TODO: Set aim sens from settings
+        m_aimSensitivity = 50f;
+
     }
 
     private void Update ()
@@ -38,22 +68,31 @@ public class CameraController : MonoBehaviour
 
     private void Look ()
     {
-        float _mouseVertical = -Input.GetAxis ( "Mouse Y" );
-        float _mouseHorizontal = Input.GetAxis ( "Mouse X" );
+        float mouseVertical = -Input.GetAxis ( "Mouse Y" );
+        float mouseHorizontal = Input.GetAxis ( "Mouse X" );
 
-        verticalRotation += _mouseVertical * sensitivity * Time.deltaTime;
-        horizontalRotation += _mouseHorizontal * sensitivity * Time.deltaTime;
+        m_verticalRotation += mouseVertical * m_sensitivity * Time.deltaTime;
+        m_horizontalRotation += mouseHorizontal * m_sensitivity * Time.deltaTime;
 
-        verticalRotation = Mathf.Clamp ( verticalRotation, -clampAngle, clampAngle );
+        m_verticalRotation = Mathf.Clamp ( m_verticalRotation, -m_clampAngle, m_clampAngle );
 
-        transform.localRotation = Quaternion.Euler ( verticalRotation, 0f, 0f );
-        player.transform.rotation = Quaternion.Euler ( 0f, horizontalRotation, 0f );
+        transform.localRotation = Quaternion.Euler ( m_verticalRotation, 0f, 0f );
+        player.transform.rotation = Quaternion.Euler ( 0f, m_horizontalRotation, 0f );
     }
 
     public void AddRecoil ( float verticalRecoil, float horizontalRecoil )
     {
-        verticalRotation -= verticalRecoil;
-        horizontalRotation -= horizontalRecoil;
+        m_verticalRotation -= verticalRecoil;
+        m_horizontalRotation -= horizontalRecoil;
+    }
+
+    /// <summary>
+    /// Called when the aim state changes.
+    /// </summary>
+    /// <param name="aimState">The new aim state.</param>
+    private void SetSensitivity ( bool aimState )
+    {
+        m_sensitivity = aimState ? m_aimSensitivity : m_normalSensitivity;
     }
 
     public void SetCursorMode ( CursorLockMode lockMode )
